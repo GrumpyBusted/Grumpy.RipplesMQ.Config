920ddcdf3d2a86abbea459e0ec99e17a3808d859
Anders Busted-Janum <abja@sdc.dk>
2018-01-21 11:38:40 +0100

    Update

---
+++ Grumpy.RipplesMQ.Config.UnitTests/PublishSubScribeConfigTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Grumpy.RipplesMQ.Config.UnitTests
+{
+    public class PublishSubScribeConfigTests
+    {
+        [Fact]
+        public void CanCreateConfig()
+        {
+            var cut = new PublishSubscribeConfig
+            {
+                Persistent = true,
+                Topic = "MyTopic"
+            };
+
+            cut.Persistent.Should().BeTrue();
+            cut.Topic.Should().Be("MyTopic");
+        }
+    }
+}
---
+++ Grumpy.RipplesMQ.Config.UnitTests/RequestResponseConfigTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Grumpy.RipplesMQ.Config.UnitTests
+{
+    public class RequestResponseConfigTests
+    {
+        [Fact]
+        public void CanCreateConfig()
+        {
+            var cut = new RequestResponseConfig
+            {
+                Name = "MyRequest",
+                MillisecondsTimeout = 1000
+            };
+
+            cut.Name.Should().Be("MyRequest");
+            cut.MillisecondsTimeout.Should().Be(1000);
+        }
+    }
+}
